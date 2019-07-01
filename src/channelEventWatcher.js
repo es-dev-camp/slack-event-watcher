573c061c19c8a9d9af20ec1452093e013a4f94a7
OiChan <makoto.uwaya@gmail.com>
2019-07-02 01:40:52 +0900

    refs #7 Write message from all 'now' channels

--- src/channelEventWatcher.js
+++ src/channelEventWatcher.js
@@ -5,6 +5,7 @@ const slack = new Slack();
 
 const slack_bot_token = process.env.BOT_TOKEN;
 const targetChannel = !slack_bot_token ? 'CKZGKHBV2' : 'CKSPDU47K';
+const streamChannel = 'CL1475Z16';
 const isDebug = !slack_bot_token;
 
 const channelEventWatcher = {
@@ -45,6 +46,39 @@ const channelEventWatcher = {
     const request = {
       token: slack_bot_token,
       channel: targetChannel, 
+      text: message
+    };
+    if (isDebug) {
+      console.log(request);
+    } else {
+      slack.chat.postMessage(request).then(console.log).catch(console.error);
+    }
+  },
+
+  now_channel_message_post: async (channelId, userId, text) => {
+    if (channelId === streamChannel) {
+      return;
+    }
+
+    const channel = {
+      token: slack_bot_token,
+      channel: channelId,
+    }
+    const channelInfo = await slack.channels.info(channel);
+    if (!channelInfo.channel.name.endsWith('_now')) {
+      return;
+    }
+
+    const user = {
+      token: slack_bot_token,
+      user: userId,
+    }
+    const userInfo = await slack.users.info(user);
+
+    const message = userInfo.user.profile.display_name + ' ' + SlackMention.channel(channelId) + '\n' + text;
+    const request = {
+      token: slack_bot_token,
+      channel: streamChannel,
       text: message
     };
     if (isDebug) {
