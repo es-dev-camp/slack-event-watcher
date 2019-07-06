8018317cb2f86e2a73b8ce80525be1e3cc17d2c2
kengop <kengop@gmail.com>
2019-07-07 01:30:39 +0900

    refs #9 use permalink for posting all_now ch.

--- src/channelEventWatcher.js
+++ src/channelEventWatcher.js
@@ -9,6 +9,7 @@ const slack_bot_token = process.env.BOT_TOKEN;
 const targetChannel = !slack_bot_token ? 'CKZGKHBV2' : 'CKSPDU47K';
 const streamChannel = 'CL1475Z16';
 const isDebug = !slack_bot_token;
+const botId = 'BKZGSLX0C';
 
 const channelEventWatcher = {
   channel_created_message_post: async (creator, channel) => {
@@ -57,36 +58,40 @@ const channelEventWatcher = {
     }
   },
 
-  now_channel_message_post: async (channelId, userId, text) => {
+  now_channel_message_post: async (event) => {
+    const channelId = event.channel;
     if (channelId === streamChannel) {
       return;
     }
 
-    const channel = {
+    const channelInfoReq = {
       token: slack_bot_token,
       channel: channelId,
     }
-    const channelInfo = await slack.channels.info(channel).catch(logger.error);
-    if (!channelInfo.channel.name.endsWith('_now')) {
+    const channelInfoRes = await slack.channels.info(channelInfoReq)
+      .catch(logger.error);
+    if (!channelInfoRes.channel.name.endsWith('_now')) {
       return;
     }
 
-    const user = {
+    const permalinkReq = {
       token: slack_bot_token,
-      user: userId,
+      channel: event.channel,
+      message_ts: event.ts
     }
-    const userInfo = await slack.users.info(user);
+    const permalinkRes = await slack.chat.getPermalink(permalinkReq)
+      .catch(logger.error);
 
-    const message = userInfo.user.profile.display_name + ' ' + SlackMention.channel(channelId) + '\n' + text;
-    const request = {
+    const postMessageReq = {
       token: slack_bot_token,
       channel: streamChannel,
-      text: message
+      text: permalinkRes.permalink,
+      unfurl_links: true
     };
     if (isDebug) {
-      logger.debug(request);
+      logger.debug(postMessageReq);
     } else {
-      slack.chat.postMessage(request).then(logger.info).catch(logger.error);
+      slack.chat.postMessage(postMessageReq).then(logger.info).catch(logger.error);
     }
   },
 };
