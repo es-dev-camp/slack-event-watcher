ae8b0c156d369f11eada340551e22749fcd49fec
kengop <kengop@gmail.com>
2021-06-07 00:45:14 +0900

    Fix logging severity

--- src/logger.js
+++ src/logger.js
@@ -4,7 +4,9 @@ module.exports = class LoggerFactory {
     if (!option && !!LoggerFactory.instance) {
       return LoggerFactory.instance;
     }
-    this.jsonPayload = {};
+    this.jsonPayload = {
+      severity: 'WARNING'
+    };
     LoggerFactory.instance = this;
     return this;
   }
@@ -12,18 +14,22 @@ module.exports = class LoggerFactory {
   create() {
     return {
       debug: (object) => {
+        this.jsonPayload.severity = 'DEBUG';
         this.jsonPayload.payload = object;
         console.debug(JSON.stringify(this.jsonPayload));
       },
       info: (object) => {
+        this.jsonPayload.severity = 'INFO';
         this.jsonPayload.payload = object;
         console.info(JSON.stringify(this.jsonPayload));
       },
       warn: (object) => {
+        this.jsonPayload.severity = 'WARNING';
         this.jsonPayload.payload = object;
         console.warn(JSON.stringify(this.jsonPayload));
       },
       error: (object) => {
+        this.jsonPayload.severity = 'ERROR';
         this.jsonPayload.payload = object;
         console.error(JSON.stringify(this.jsonPayload));
       }
